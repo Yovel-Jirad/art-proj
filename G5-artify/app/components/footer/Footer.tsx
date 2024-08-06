1b093ad2afc4951e5b1f26f2643ad8e97130f976
Shwayke <120161892+Shwayke@users.noreply.github.com>
2024-08-06 22:19:17 +0000

    added footer tw to globals.css

--- G5-artify/app/components/footer/Footer.tsx
+++ G5-artify/app/components/footer/Footer.tsx
@@ -5,20 +5,20 @@ const Footer: React.FC = () => {
   return (
     <footer className=" text-sm mt-16 relative">
       <Container>
-        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8 relative z-10">
-        <div className="w-full md:w-1/3 mb-6 md:mb-0">
-            <h3 className="text-base font-bold mb-2">About Us</h3>
-            <p className="w-full mb-2 md:mb-0">
+        <div className="footer">
+          <div className="footer-about-us">
+            <h3 className="footer-about-us-title">About Us</h3>
+            <p className="footer-about-us-text">
               At Artify, we make it our mission to help you discover and buy from the best emerging artists around the world.
             </p>
             <p>&copy; {new Date().getFullYear()} Artify. All rights reserved</p>
           </div>  
           <div>
-            <h3 className="text-base font-bold mb-2">Creators</h3>
-              <p className="mb-2">Shay Pinsky</p>
-              <p className="mb-2">Yovel Jirad</p>
-              <p className="mb-2">Adar Budomski</p>
-              <p className="mb-2">Ariel Dahan</p>  
+            <h3 className="footer-creators">Creators</h3>
+            <p className="mb-1">Shay Pinsky</p>
+            <p className="mb-1">Yovel Jirad</p>
+            <p className="mb-1">Adar Budomski</p>
+            <p className="mb-1">Ariel Dahan</p>  
           </div>    
         </div>
       </Container>
